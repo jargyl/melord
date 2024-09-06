e1fd990b8b09b6b379e6a2b95022ab7824948d92
jargyl <jari.gielen@gmail.com>
2024-09-07 00:18:18 +0200

    opened local network px to rem mobile sidebar in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import Logo from "../assets/navbar/logo.png";
 import Menu from "../assets/navbar/menu.svg";
+import Close from "../assets/navbar/close.svg";
+
+const menuItems = [
+  { label: "TRACKS", href: "#" },
+  { label: "LICENSE", href: "#" },
+  { label: "VIDEOS", href: "#" },
+  { label: "CONTACT", href: "#" },
+];
+
+const NavbarLinks = ({ className }) => (
+  <>
+    {menuItems.map((item, index) => (
+      <li key={index}>
+        <a href={item.href} className={className}>
+          {item.label}
+        </a>
+      </li>
+    ))}
+    <li>
+      <a href="#" className="btn btn-primary">
+        SHOP
+      </a>
+    </li>
+  </>
+);
 
 const Navbar = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
@@ -11,36 +42,27 @@ const Navbar = () => {
           <img src={Logo} alt="Logo" className="logo" />
         </div>
         <ul className="navbar-menu">
-          <li>
-            <a href="#" className="navbar-link">
-              TRACKS
-            </a>
-          </li>
-          <li>
-            <a href="#" className="navbar-link">
-              LICENSE
-            </a>
-          </li>
-          <li>
-            <a href="#" className="navbar-link">
-              VIDEOS
-            </a>
-          </li>
-          <li>
-            <a href="#" className="navbar-link">
-              CONTACT
-            </a>
-          </li>
-          <li>
-            <a href="#" className="btn btn-primary cool-effect">
-              SHOP
-            </a>
-          </li>
-          <li className="menu-icon">
+          <NavbarLinks className="navbar-link" />
+          <li className="menu-icon" onClick={toggleSidebar}>
             <img src={Menu} alt="menu" />
           </li>
         </ul>
       </div>
+      <div className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
+        <div className="sidebar-header">
+          <img src={Logo} alt="Logo" className="logo" />
+          <img
+            src={Close}
+            alt="Close"
+            className="close-icon"
+            onClick={toggleSidebar}
+          />
+        </div>
+        <ul className="sidebar-menu">
+          <NavbarLinks className="sidebar-link" />
+        </ul>
+      </div>
+      {isSidebarOpen && <div className="overlay" onClick={toggleSidebar}></div>}
     </nav>
   );
 };
