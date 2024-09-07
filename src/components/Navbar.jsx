1aec5de8054b8e57bf13ec7073bfcfde8c9f8df1
jargyl <jari.gielen@gmail.com>
2024-09-07 16:13:30 +0200

    mobile sidebar new layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,21 @@ import "./Navbar.css";
 import Logo from "../assets/navbar/logo.png";
 import Menu from "../assets/navbar/menu.svg";
 import Close from "../assets/navbar/close.svg";
+import Spotify from "../assets/navbar/spotify.svg";
+import Instagram from "../assets/navbar/instagram.svg";
+import Discord from "../assets/navbar/discord.svg";
 
 const menuItems = [
   { label: "TRACKS", href: "#" },
   { label: "LICENSE", href: "#" },
   { label: "VIDEOS", href: "#" },
   { label: "CONTACT", href: "#" },
+];
+
+const socialIcons = [
+  { name: "Spotify", icon: Spotify, href: "#" },
+  { name: "Instagram", icon: Instagram, href: "#" },
+  { name: "Discord", icon: Discord, href: "#" },
 ];
 
 const NavbarLinks = ({ className }) => (
@@ -20,11 +29,6 @@ const NavbarLinks = ({ className }) => (
         </a>
       </li>
     ))}
-    <li>
-      <a href="#" className="btn btn-primary">
-        SHOP
-      </a>
-    </li>
   </>
 );
 
@@ -43,6 +47,11 @@ const Navbar = () => {
         </div>
         <ul className="navbar-menu">
           <NavbarLinks className="navbar-link" />
+          <li>
+            <a href="#" className="btn btn-primary">
+              SHOP
+            </a>
+          </li>
           <li className="menu-icon" onClick={toggleSidebar}>
             <img src={Menu} alt="menu" />
           </li>
@@ -50,17 +59,28 @@ const Navbar = () => {
       </div>
       <div className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
         <div className="sidebar-header">
-          <img src={Logo} alt="Logo" className="logo" />
-          <img
-            src={Close}
-            alt="Close"
-            className="close-icon"
-            onClick={toggleSidebar}
-          />
+          <img src={Logo} alt="Logo" className="sidebar-logo" />
+          <div className="sidebar-close" onClick={toggleSidebar}>
+            <img src={Close} alt="Close" className="close-icon" />
+          </div>
         </div>
-        <ul className="sidebar-menu">
-          <NavbarLinks className="sidebar-link" />
-        </ul>
+        <div className="sidebar-content">
+          <ul className="sidebar-menu">
+            <NavbarLinks className="sidebar-link" />
+          </ul>
+          <div className="social-icons">
+            {socialIcons.map((icon, index) => (
+              <a
+                key={index}
+                href={icon.href}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={icon.icon} alt={icon.name} className="social-icon" />
+              </a>
+            ))}
+          </div>
+        </div>
       </div>
       {isSidebarOpen && <div className="overlay" onClick={toggleSidebar}></div>}
     </nav>
