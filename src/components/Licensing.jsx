3f307a03fd4f6630a2868c8964a0f29733e2e737
jargyl <jari.gielen@gmail.com>
2024-09-15 20:33:47 +0200

    updated breakpoints

--- src/components/Licensing.jsx
+++ src/components/Licensing.jsx
@@ -67,7 +67,7 @@ const Licensing = () => {
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth < 768);
+      setIsMobile(window.innerWidth < 1024);
     };
 
     handleResize();
