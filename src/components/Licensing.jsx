bdd4235442abb4c263a78d0b450d907461207d45
jargyl <jari.gielen@gmail.com>
2024-09-15 22:03:40 +0200

    Fixed typo that caused deploy issues

--- src/components/Licensing.jsx
+++ src/components/Licensing.jsx
@@ -3,7 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
-import "./licensing.css";
+import "./Licensing.css";
 import Checklist from "../assets/licensing/checklist.svg";
 import { LicensingCard } from "./LicensingCard";
 import LicensePopup from "./LicensePopup";
